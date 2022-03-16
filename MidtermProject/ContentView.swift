56c8341b80badf721d00947db7e8209b229add09
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-03-16 17:06:08 -0400

    Initial Commit

---
+++ MidtermProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MidtermProject
+//
+//  Created by Jordyn Fulbright on 3/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MidtermProject/MidtermProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermProjectApp.swift
+//  MidtermProject
+//
+//  Created by Jordyn Fulbright on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MidtermProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
